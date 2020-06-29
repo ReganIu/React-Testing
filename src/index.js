12689c06c5522642c930b1da25d42984d809394b
Regan Iu <reganiu@hotmail.com>
2020-06-28 20:47:59 -0400

    unordered list test

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello world!</h1>, document.getElementById('root'));
+//JSX
+ReactDOM.render(
+<div>
+  <ul>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+  </ul>
+</div>, document.getElementById('root'));
