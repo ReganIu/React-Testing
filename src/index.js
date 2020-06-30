844d753dda42014bd03e1c90babda867eb6c1b1f
Regan Iu <reganiu@hotmail.com>
2020-06-30 04:28:06 -0400

    moved component file into its own folder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import MyInfo from "./MyInfo" //default file type is .js
+import MyInfo from "./components/MyInfo" //default file type is .js
 
 //functional component below uses capital first letter
 //component can only return 1 JSX element, can get around by wrapping 2 or more elements in a <div>
