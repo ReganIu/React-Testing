b95a21d493bee720960c105fa4e781793f7e9579
Regan Iu <reganiu@hotmail.com>
2020-07-08 00:19:16 -0400

    inline css styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,24 @@
 import React from "react"
 
 function Header() {
+
+    const styles = {
+        color: '#FF8C00', 
+        backgroundColor: '#FF2D00',
+        fontSize: 35
+    }
+    
     return (
-        <nav>
+        <header className="navbar">
         <h1> This is a website!</h1>
-          <ul>
+          <ul style={styles}>
             <li>Welcome to my website!</li>
             <li>Please checkout my projects:</li>
           </ul>
-      </nav>
+
+      </header>
     )
 }
+
 
 export default Header
