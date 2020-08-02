79f4cd06f3752fb21030abed3a53916ab88e4ac5
Regan Iu <reganiu@hotmail.com>
2020-08-01 22:29:17 -0400

    learning use of props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from "react"
-
-import Header from "./components/Header"
-import MainContent from "./components/MainContent"
-import Footer from "./components/Footer"
+import ContactCard from "./ContactCard"
 
 function App() {
   return (
     <div>
-      <Header/>
-      <MainContent/>
-      <Footer/>
+      <ContactCard name="bob" phone="416" email ="123@asdf.com"/>
+      <ContactCard name="bobby" phone="567" email ="345@asdf.com"/>
+      <ContactCard name="bobbie" phone="345" email ="456@asdf.com"/>
+      <ContactCard name="boby" phone="234" email ="567@asdf.com"/>
     </div>
   )
 }
