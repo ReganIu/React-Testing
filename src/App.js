7137c5708ee58a4417e9ad32b5e836ec4a593e46
Regan Iu <reganiu@hotmail.com>
2020-08-01 22:36:30 -0400

    simplified prop

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import ContactCard from "./ContactCard"
 function App() {
   return (
     <div>
-      <ContactCard name="bob" phone="416" email ="123@asdf.com"/>
-      <ContactCard name="bobby" phone="567" email ="345@asdf.com"/>
-      <ContactCard name="bobbie" phone="345" email ="456@asdf.com"/>
-      <ContactCard name="boby" phone="234" email ="567@asdf.com"/>
+      <ContactCard contact= {{name:"bob", phone:"416", email :"123@asdf.com"}}/>
+      <ContactCard contact={{name:"bobby", phone:"567", email :"345@asdf.com"}}/>
+      <ContactCard contact={{name:"bobbie", phone:"345", email :"456@asdf.com"}}/>
+      <ContactCard contact={{name:"boby", phone:"234", email :"567@asdf.com"}}/>
     </div>
   )
 }
