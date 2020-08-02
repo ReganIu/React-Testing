c48d20cb07be32c61741afd195578df738bba40a
Regan Iu <reganiu@hotmail.com>
2020-08-01 23:42:31 -0400

    working with props and inline styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react"
-import ContactCard from "./ContactCard"
+import Joke from "./Joke"
 
 function App() {
   return (
     <div>
-      <ContactCard contact= {{name:"bob", phone:"416", email :"123@asdf.com"}}/>
-      <ContactCard contact={{name:"bobby", phone:"567", email :"345@asdf.com"}}/>
-      <ContactCard contact={{name:"bobbie", phone:"345", email :"456@asdf.com"}}/>
-      <ContactCard contact={{name:"boby", phone:"234", email :"567@asdf.com"}}/>
+      <Joke punchline = "haha" />
+      <Joke question = "hello" punchline="asdfasdf" />
+      <Joke question = "hello" punchline="asdfasdf" />
+      <Joke question = "hello" punchline="asdfasdf" />
+      <Joke question = "hello" punchline="asdfasdf" />
+      <Joke question = "hello" punchline="asdfasdf" />  
     </div>
   )
 }
