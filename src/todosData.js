e6b779aeda7832800c7e3671c8b883f05944a662
Regan Iu <reganiu@hotmail.com>
2020-08-06 01:44:09 -0400

    created data file to retrieve

---
+++ src/todosData.js
@@ -0,0 +1,29 @@
+const todosData = [
+    {
+        id: 1,
+        text: "Take out the trash",
+        completed: true
+    },
+    {
+        id: 2,
+        text: "Buy groceries",
+        completed: false
+    },
+    {
+        id: 3,
+        text: "Feed cat",
+        completed: false
+    },
+    {
+        id: 4,
+        text: "Watch shows",
+        completed: true
+    },
+    {
+        id: 5,
+        text: "Do homework",
+        completed: false
+    },
+]
+
+export default todosData
