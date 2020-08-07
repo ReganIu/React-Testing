e70e30511f1ad1a4c04493960b20c4249f8e37a5
Regan Iu <reganiu@hotmail.com>
2020-08-06 23:10:30 -0400

    adding state to code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react"
-import TodoItem from "./TodoItem"
-import todosData from "./todosData"
+//import TodoItem from "./TodoItem"
+//import todosData from "./todosData"
 
 // function App() {
 //   const todoItems = todosData.map(item => <TodoItem key={item.id} item={item}/>)
@@ -13,14 +13,29 @@ import todosData from "./todosData"
 // }
 
 
-//class component
+// class component
 class App extends React.Component {
-  render() {
-    const todoItems = todosData.map(item => <TodoItem key={item.id} item={item}/>)
+//   render() {
+//     const todoItems = todosData.map(item => <TodoItem key={item.id} item={item}/>)
 
-    return (
-      <div className = "todo-list">
-        {todoItems}
+//     return (
+//       <div className = "todo-list">
+//         {todoItems}
+//       </div>
+//     )
+//   }
+
+//constructor method
+  constructor () {
+    super()
+    this.state = {
+      answer: 'Yes'
+    }
+  }
+  render() {
+    return ( 
+      <div> 
+        <h1> What is state? {this.state.answer} </h1>
       </div>
     )
   }
