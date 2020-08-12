a8cc331482d7db186d38da9fccd78d917f5ee7de
Regan Iu <reganiu@hotmail.com>
2020-08-12 01:00:35 -0400

    changed code to not directly modify state object

--- src/App.js
+++ src/App.js
@@ -54,8 +54,11 @@ class App extends React.Component {
     handleChange (id) {
       this.setState(prevState => {
           const updated = prevState.todos.map(todo => {
-            if (todo.id == id) {
-              todo.completed = !todo.completed
+            if (todo.id === id) {
+              return {
+                ...todo, 
+                completed: !todo.completed
+              }
             }
             return todo
           })
