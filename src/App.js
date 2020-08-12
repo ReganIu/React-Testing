c214c38b5a26b22e82085d3bf64475bce59dd9ec
Regan Iu <reganiu@hotmail.com>
2020-08-12 00:59:31 -0400

    added handleChange function to change states

--- src/App.js
+++ src/App.js
@@ -48,9 +48,25 @@ class App extends React.Component {
       this.state = {
         todos: todosData
       }
+      this.handleChange = this.handleChange.bind(this)
     }
+    
+    handleChange (id) {
+      this.setState(prevState => {
+          const updated = prevState.todos.map(todo => {
+            if (todo.id == id) {
+              todo.completed = !todo.completed
+            }
+            return todo
+          })
+          return {
+            todos: updated
+          }
+      })
+    }
+    
     render() {
-      const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item = {item}/>)
+      const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item = {item} handleChange = {this.handleChange}/>)
       
       return (
           <div className='todo-list'>
